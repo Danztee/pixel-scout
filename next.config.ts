0339d464b9fe5a2be186dc25cbb03307d0a59597
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-04-20 10:52:43 +0100

    fix: babel config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    // Enable SWC compiler
+    styledComponents: true,
+  },
+  experimental: {
+    // Disable Babel completely
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;

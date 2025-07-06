c7ab67c63cda68c83f5d1401d76035371e447e6c
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-07-06 21:58:52 +0100

    refactor: update start script to include database migration and remove obsolete user migration files

--- migrations/20250706205142_premium_skreet.sql
+++ migrations/20250706205142_premium_skreet.sql
@@ -8,3 +8,6 @@ CREATE TABLE "users" (
 	CONSTRAINT "users_email_unique" UNIQUE("email"),
 	CONSTRAINT "users_username_unique" UNIQUE("username")
 );
+--> statement-breakpoint
+CREATE INDEX "email_index" ON "users" USING btree ("email");--> statement-breakpoint
+CREATE INDEX "username_index" ON "users" USING btree ("username");
